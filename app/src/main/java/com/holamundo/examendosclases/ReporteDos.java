8504204e1c74d85e6696388fa2b0a42e00fdf5bb
Andres Tarra <afelipe0113@gmail.com>
2018-09-13 20:02:41 -0500

    Interfaz terminada sin funcionalidad

---
+++ app/src/main/java/com/holamundo/examendosclases/ReporteDos.java
@@ -0,0 +1,13 @@
+package com.holamundo.examendosclases;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ReporteDos extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reporte_dos);
+    }
+}
